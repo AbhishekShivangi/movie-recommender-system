bc93cff2eb0fe25ca4e0fc2659ef648bc2b6e4d3
Abhishek Shivangi <131172976+AbhishekShivangi@users.noreply.github.com>
2026-03-12 20:25:26 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -2,31 +2,33 @@ import pickle
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
-movies = pickle.load(open("movies_list.pkl","rb"))
+movies = pickle.load(open("movies_list.pkl", "rb"))
 movies = pd.DataFrame(movies)
 
-vectors = pickle.load(open("movie_vectors.pkl","rb"))
+vectors = pickle.load(open("movie_vectors.pkl", "rb"))
 
 similarity = cosine_similarity(vectors)
 
+
 def recommend(movie):
 
-    index=movies[movies['title']==movie].index[0]
+    index = movies[movies['title'] == movie].index[0]
 
-    distances=similarity[index]
+    distances = similarity[index]
 
-    movie_indices=sorted(
+    movie_indices = sorted(
         list(enumerate(distances)),
         reverse=True,
-        key=lambda x:x[1]
+        key=lambda x: x[1]
     )[1:6]
 
-    movie_titles=[]
-    movie_ids=[]
+    names = []
+    ids = []
 
     for i in movie_indices:
 
-        movie_titles.append(movies.iloc[i[0]].title)
-        movie_ids.append(movies.iloc[i[0]].id)
+        names.append(movies.iloc[i[0]].title)
 
-    return movie_titles,movie_ids
+        ids.append(movies.iloc[i[0]].id)
+
+    return names, ids
