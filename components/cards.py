a200e5b517092876f44739da0f77214162990dc2
Abhishek Shivangi <131172976+AbhishekShivangi@users.noreply.github.com>
2026-03-12 21:55:39 +0530

    Create cards.py

---
+++ components/cards.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+IMG="https://image.tmdb.org/t/p/w500"
+
+def movie_row_scroll(title,movies):
+
+    st.subheader(title)
+
+    html = "<div class='movie-row'>"
+
+    for m in movies[:20]:
+
+        if m.get("poster_path"):
+
+            poster = IMG + m["poster_path"]
+
+            html += f"""
+            <div class="movie-card">
+                <img src="{poster}">
+                <p style="color:white;text-align:center">
+                {m.get("title") or m.get("name")}
+                </p>
+            </div>
+            """
+
+    html += "</div>"
+
+    st.markdown(html, unsafe_allow_html=True)
