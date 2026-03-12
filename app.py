d2cf4293ae231c66064cd93592b17829d9dcb57b
Abhishek Shivangi <131172976+AbhishekShivangi@users.noreply.github.com>
2026-03-12 17:38:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import pickle
 import requests
 import certifi
 import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 
 st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
 
@@ -12,22 +10,10 @@ st.title("🎬 Movie Recommender System")
 st.write("Find movies similar to your favorite ones")
 
 # Load movies data
-movies = pickle.load(open("movies_list.pkl", "rb"))
-
-# Convert to DataFrame
+movies = pickle.load(open("movies_list.pkl","rb"))
 movies = pd.DataFrame(movies)
 
-# Convert tags safely to string
-movies['tags'] = movies['tags'].astype(str)
-
 movie_list = movies['title'].values
-
-# Create vectors
-cv = CountVectorizer(max_features=5000, stop_words='english')
-vectors = cv.fit_transform(movies['tags']).toarray()
-
-# Calculate similarity
-similarity = cosine_similarity(vectors)
 
 
 @st.cache_data
@@ -39,36 +25,13 @@ def fetch_poster(movie_id):
         data = requests.get(url, verify=certifi.where()).json()
         poster_path = data.get("poster_path")
 
-        return "https://image.tmdb.org/t/p/w500/" + poster_path
+        if poster_path:
+            return "https://image.tmdb.org/t/p/w500/" + poster_path
+        else:
+            return "https://via.placeholder.com/500x750"
 
     except:
         return "https://via.placeholder.com/500x750"
-
-
-def recommend(movie):
-
-    index = movies[movies['title'] == movie].index[0]
-
-    distances = similarity[index]
-
-    movie_list = sorted(
-        list(enumerate(distances)),
-        reverse=True,
-        key=lambda x: x[1]
-    )[1:6]
-
-    recommended_movies = []
-    recommended_posters = []
-
-    for i in movie_list:
-
-        movie_id = movies.iloc[i[0]].id
-
-        recommended_movies.append(movies.iloc[i[0]].title)
-
-        recommended_posters.append(fetch_poster(movie_id))
-
-    return recommended_movies, recommended_posters
 
 
 selected_movie = st.selectbox(
@@ -76,28 +39,10 @@ selected_movie = st.selectbox(
     movie_list
 )
 
-if st.button("Recommend Movies"):
+if st.button("Show Movie Poster"):
 
-    names, posters = recommend(selected_movie)
+    movie_id = movies[movies['title']==selected_movie].iloc[0].id
 
-    col1, col2, col3, col4, col5 = st.columns(5)
+    poster = fetch_poster(movie_id)
 
-    with col1:
-        st.image(posters[0])
-        st.caption(names[0])
-
-    with col2:
-        st.image(posters[1])
-        st.caption(names[1])
-
-    with col3:
-        st.image(posters[2])
-        st.caption(names[2])
-
-    with col4:
-        st.image(posters[3])
-        st.caption(names[3])
-
-    with col5:
-        st.image(posters[4])
-        st.caption(names[4])
+    st.image(poster)
