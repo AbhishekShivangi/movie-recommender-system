20d9f00c4b12a8d635385957e631009de216a14c
Abhishek Shivangi <131172976+AbhishekShivangi@users.noreply.github.com>
2026-03-12 21:58:56 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,30 +1,29 @@
 import streamlit as st
 
-IMG="https://image.tmdb.org/t/p/w500"
+def hero_banner():
 
-def hero():
     st.markdown(
-        """
-        <div style="background:black;padding:40px;border-radius:10px">
-        <h1 style="color:red">🎬 Go Movie Discovery</h1>
-        <p style="color:white">Discover movies, actors, series, anime</p>
-        </div>
-        """,
-        unsafe_allow_html=True
+    """
+    <div class='hero-banner'>
+        <h1 style='color:red'>🎬 Go Movie Discovery</h1>
+        <h3 style='color:white'>
+        Discover movies, series, anime and actors
+        </h3>
+    </div>
+    """,
+    unsafe_allow_html=True
     )
 
-def movie_row(title,movies):
+def trailer_preview(url):
 
-    st.subheader(title)
+    if url:
 
-    cols=st.columns(5)
+        html=f"""
+        <iframe width="100%" height="400"
+        src="{url}?autoplay=1&mute=1"
+        frameborder="0"
+        allow="autoplay">
+        </iframe>
+        """
 
-    for i,m in enumerate(movies[:10]):
-
-        with cols[i%5]:
-
-            if m.get("poster_path"):
-
-                st.image(IMG+m["poster_path"])
-
-                st.caption(m.get("title") or m.get("name"))
+        st.markdown(html,unsafe_allow_html=True)
