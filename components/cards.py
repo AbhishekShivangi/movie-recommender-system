9927e9f847abecc3617cc8a804e710f07d7fefb5
Abhishek Shivangi <131172976+AbhishekShivangi@users.noreply.github.com>
2026-03-12 22:09:07 +0530

    Update cards.py

--- components/cards.py
+++ components/cards.py
@@ -1,28 +1,28 @@
 import streamlit as st
 
-IMG="https://image.tmdb.org/t/p/w500"
+IMG = "https://image.tmdb.org/t/p/w500"
 
-def movie_row_scroll(title,movies):
+def movie_row_scroll(title, movies):
 
     st.subheader(title)
 
-    html="<div class='movie-row'>"
+    html = "<div class='movie-row'>"
 
     for m in movies[:20]:
 
         if m.get("poster_path"):
 
-            poster=IMG+m["poster_path"]
+            poster = IMG + m["poster_path"]
 
-            html+=f"""
-            <div class='movie-card'>
-            <img src='{poster}'>
-            <p style='color:white;text-align:center'>
-            {m.get("title") or m.get("name")}
-            </p>
+            html += f"""
+            <div class="movie-card">
+                <img src="{poster}">
+                <p style="color:white;text-align:center">
+                {m.get("title") or m.get("name")}
+                </p>
             </div>
             """
 
-    html+="</div>"
+    html += "</div>"
 
-    st.markdown(html,unsafe_allow_html=True)
+    st.markdown(html, unsafe_allow_html=True)
