ab88e94376fd76740124d4a8b1a68be81249790a
Abhishek Shivangi <131172976+AbhishekShivangi@users.noreply.github.com>
2026-03-12 17:23:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,59 +4,51 @@ import requests
 import certifi
 from sklearn.metrics.pairwise import cosine_similarity
 
-st.set_page_config(page_title="Movie Recommender System", page_icon="🎬", layout="wide")
+st.set_page_config(page_title="Movie Recommender", page_icon="🎬", layout="wide")
 
 st.title("🎬 Movie Recommender System")
 st.write("Find movies similar to your favorite ones")
 
-# Load data
+# Load movies
 movies = pickle.load(open("movies_list.pkl", "rb"))
-similarity = pickle.load(open("similarity.pkl", "rb"))   # small file instead of similarity.pkl
 
-movies_list = movies['title'].values
+# Create vectors from movie features
+vectors = movies['tags']
 
-
-# Fetch movie poster
+# Fetch poster
 @st.cache_data
 def fetch_poster(movie_id):
-
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US"
-
     try:
         data = requests.get(url, verify=certifi.where()).json()
         poster_path = data.get("poster_path")
-
-        if poster_path:
-            return "https://image.tmdb.org/t/p/w500/" + poster_path
-        else:
-            return "https://via.placeholder.com/500x750?text=No+Image"
-
+        return "https://image.tmdb.org/t/p/w500/" + poster_path
     except:
-        return "https://via.placeholder.com/500x750?text=Error"
+        return "https://via.placeholder.com/500x750"
 
 
 def recommend(movie):
-
     index = movies[movies['title'] == movie].index[0]
 
     similarity = cosine_similarity([vectors[index]], vectors)[0]
 
-    movie_list = sorted(list(enumerate(similarity)), reverse=True, key=lambda x: x[1])[1:6]
+    movie_list = sorted(list(enumerate(similarity)),
+                        reverse=True,
+                        key=lambda x: x[1])[1:6]
 
-    recommended_movies = []
-    recommended_posters = []
+    names = []
+    posters = []
 
     for i in movie_list:
+        movie_id = movies.iloc[i[0]].id
+        names.append(movies.iloc[i[0]].title)
+        posters.append(fetch_poster(movie_id))
 
-        movie_id = movies.iloc[i[0]]['id']
-
-        recommended_movies.append(movies.iloc[i[0]]['title'])
-        recommended_posters.append(fetch_poster(movie_id))
-
-    return recommended_movies, recommended_posters
+    return names, posters
 
 
-selected_movie = st.selectbox("Select a movie", movies_list)
+movie_list = movies['title'].values
+selected_movie = st.selectbox("Select movie", movie_list)
 
 if st.button("Recommend Movies"):
 
@@ -68,4 +60,3 @@ if st.button("Recommend Movies"):
         with cols[i]:
             st.image(posters[i])
             st.caption(names[i])
-
